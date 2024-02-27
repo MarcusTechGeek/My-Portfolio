df63131944f6b6be995b13928bdd61134fccda6e
MarcusTechGeek <marcusgouws93@gmail.com>
2024-02-27 12:40:13 +0200

    blank test

--- Kids_Book/Chapter1/chapter1.js
+++
@@ -1,9 +0,0 @@
-// Draw as many cats as you want!
-/* this comment
-stretches over several lines*/
-let drawCats = function (howmanytimes) {
-    for (let i = 0; i < howmanytimes; i++) {
-        console.log(i + " =^.^= ");
-    }
-};
-drawCats(10);// you can put in any number instead of 10  
